d68f10c2e9de864dd3ec948a4bb24cdd404a5a84
thomju14 <73489243+thomju14@users.noreply.github.com>
2023-05-16 00:02:48 -0600

    Updated - design

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@
 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import logo from './Pictures/logo.png';
 
 function App() {
   const [textToValidate, setTextToValidate] = useState('');
-  const [fakePercentage, setFakePercentage] = useState('');
   const [numCharacters, setNumCharacters] = useState('');
   const [numWords, setNumWords] = useState('');
+  const [resultText, setResultText] = useState(false);
 
   useEffect(() => {
     setNumCharacters(textToValidate.length);
   }, [textToValidate]);
-  
+
   async function postData() {
     const options = {
       method: 'POST',
@@ -29,14 +30,14 @@ function App() {
       data: {
         text: textToValidate
       }
-    };
+    };         
 
 
     try {
       const response = await axios.request(options);
       console.log(response.data);
-      setFakePercentage(response.data.fakePercentage)
-      setNumWords(response.data.textWords)
+      setNumWords(` ${response.data.textWords} words`)
+      setResultText(`Your text is ${response.data.fakePercentage}% fake and ${100 - response.data.fakePercentage}% likely to be created by a human.`);
     } catch (error) {
       console.error(error);
     }
@@ -48,20 +49,26 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>Include your text here (min 250 characters)</label>
+    <div><img class="w-96 h-48 max-h-full" src={logo} alt="Logo"  />'
+    <div class=" m-24"  >
+      
+      <form   onSubmit={handleSubmit}>
+        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 ">Text Detect</label>
+    
+        <textarea class="block p-2.5 w-3/4 h-48 max-h-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="message" onChange={(e) => setTextToValidate(e.target.value)} placeholder="Include your text here (min 250 characters)"></textarea>
         <br />
-        <textarea style={{width: '400px', height: '200px'}} id="yourText" onChange={(e) => setTextToValidate(e.target.value)}></textarea>
-        <br />
-        <button type="submit">Submit</button>
+        <button class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800" type="submit">Submit</button>
       </form>
       <div>
         <div id='numCharacters' className='results'>{numCharacters} characters</div>
-        <div id='fakePercentage' className='results'>Your text is {fakePercentage}% fake and {100- fakePercentage}% likely to be created by a human.</div>
-        <div id='numWords' className='results'>{numWords} words</div>
+     
+          {resultText && <div className='results'>{numWords} </div>}
+          {resultText && <div className='results'>{resultText}</div>}
       </div>
     </div>
+    
+    </div>
+
   );
 }
 
